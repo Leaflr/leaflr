f27ef1c451a9ac1f4e9538585b806def4a25dc91
Kaleb Clark <kaleb@abraxxus.net>
2014-01-03 21:24:49 -0800

    Added onOpen event to choice view - Added to survey JSON. Cleaned up choice-view.js

--- app/scripts/survey/views/choice-view.js
+++ app/scripts/survey/views/choice-view.js
@@ -6,37 +6,40 @@ function( Backbone, Communicator, choiceTemp ){
 	'use strict';
 
 	return Backbone.Marionette.ItemView.extend({
-  		template: choiceTemp,
-      className: 'choice',
+        template: choiceTemp,
+        className: 'choice',
 
   		events: {
   			'click' : 'selected'
   		},
+        onBeforeRender: function() {
+            if(this.model.get('onOpen'))    
+                this.model.attributes.onOpen( this.model );
+        },
 
   		selected: function(){
-        var self = this;
-        var theme = self.model.get('theme');
+            var self = this;
+            var theme = self.model.get('theme');
 
-        /* set answer to step model */
-        this.model.step.attributes.answer = this.model.attributes.name;
+            /* set answer to step model */
+            this.model.step.attributes.answer = this.model.attributes.name;
 
-        // add CSS animation classes
-        this.$el.find('.icon').addClass('checked');
-        this.$el.find('.icon-svg').addClass('checked').attr('src','themes/'+theme+'/images/icons/checkmark.svg');
-        this.$el.siblings().find('.icon-wrapper').addClass('not-checked');
+            // add CSS animation classes
+            this.$el.find('.icon').addClass('checked');
+            this.$el.find('.icon-svg').addClass('checked').attr('src','themes/'+theme+'/images/icons/checkmark.svg');
+            this.$el.siblings().find('.icon-wrapper').addClass('not-checked');
 
-        window.setTimeout(function(){
-          // make step as completed
-          self.model.step.set('completed', self.model);
-          self.$el.closest('.step').attr('class','step reset-animation slide-out');
+            window.setTimeout(function() {
+                // make step as completed
+                self.model.step.set('completed', self.model);
+                self.$el.closest('.step').attr('class','step reset-animation slide-out');
 
-          // trigger event to render next step
-          Communicator.events.trigger('nextStep', self.model.get('nextStep') );
-          
-        }, 500); 
+                // trigger event to render next step
+                Communicator.events.trigger('nextStep', self.model.get('nextStep') );
+            }, 500); 
 
-  			if (this.model.get('onSelect'))
-  			this.model.attributes.onSelect( this.model );
+            if (this.model.get('onSelect'))
+            this.model.attributes.onSelect( this.model );
 
   		}
 	});
